99062a4ef97b78d7f95d2a3838655b1083161860
allison <allison.durham@gmail.com>
2022-01-27 02:51:35 -0500

    deploy issue

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,10 +17,7 @@ export default function Home() {
           <div class="">
             <h1 class="">Home</h1>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
+                Easy Tiger is our band, we play music really good!  people like us. one fan at a time!
             </p>
           </div>
         </div>
