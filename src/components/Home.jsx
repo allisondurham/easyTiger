97ec0771a5640a8020633a92e776be65c5d2e280
allison <allison.durham@gmail.com>
2022-01-28 01:37:48 -0500

    stripes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,14 +7,14 @@ export default function Home() {
     return (
         <div className="home mt-24 md:mt-16 mb-0 pb-10 bg-yellow-700 ">
             <div className="pt-8 px-4 md:p-10 lg:pt-6 pb-20">
-            <div class="container bg-stone-800  rounded-2xl ">
+            <div class="container bg-stone-800/90 p-4 rounded-2xl ">
                 <img
                     class="rounded-2xl "
                         src={image}
                         alt=""
                     />
-                <p className="text-yellow-700 py-4">
-                    Easy Tiger is our band, we play music real good!  people like us. one fan at a time!
+                <p className="text-yellow-700  py-4">
+                    Easy Tiger is our band, we play music.  people like us. 
                 </p>
             </div>      
             </div>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -8,24 +8,24 @@ export default function Navbar({ fixed }) {
                 <div className="container  mx-auto flex flex-wrap items-center">
                     <div className="flex w-full md:w-1/2 justify-center md:justify-start font-extrabold">
                         <a className="no-underline hover:text-cyan hover:no-underline" href="/">
-                            <span className="text-4xl text-yellow-700 pl-2 hover:text-yellow-500" id="">
+                            <span className="text-4xl text-amber-600 pl-2 hover:text-amber-500" id="">
                                 <i  className="em em-grinning"></i> Easy Tiger
                             </span>
                         </a>
                     </div>
                     <div className="flex w-full pt-2 content-center justify-between md:w-1/2 md:justify-end">
-                        <ul className="list-reset flex justify-between flex-1 text-yellow-700  md:flex-none items-center">
+                        <ul className="list-reset flex justify-between flex-1 text-amber-600  md:flex-none items-center">
                             <li className="mr-3">
-                                <a id="nav" className="inline-block text-xl font-semibold  no-underline hover:text-yellow-500 hover:text-underline py-2 px-2" href="/">home</a>
+                                <a id="nav" className="inline-block text-xl font-semibold  no-underline hover:text-amber-500 hover:text-underline py-2 px-2" href="/">home</a>
                             </li>
                             <li className="mr-3">
-                                <a id="nav" className="inline-block text-xl font-semibold  no-underline hover:text-yellow-500 hover:text-underline py-2 px-2" href="/about">about</a>
+                                <a id="nav" className="inline-block text-xl font-semibold  no-underline hover:text-amber-500 hover:text-underline py-2 px-2" href="/about">about</a>
                             </li>
                             <li className="mr-3">
-                                <a id="nav" className="inline-block text-xl font-semibold  no-underline hover:text-yellow-500 hover:text-underline py-2 px-2" href="/music">music</a>
+                                <a id="nav" className="inline-block text-xl font-semibold  no-underline hover:text-amber-500 hover:text-underline py-2 px-2" href="/music">music</a>
                             </li>
                             <li className="mr-3">
-                                <a id="nav" className="inline-block text-xl font-semibold  no-underline hover:text-yellow-500 hover:text-underline py-2 px-2" href="/tour">tour</a>
+                                <a id="nav" className="inline-block text-xl font-semibold  no-underline hover:text-amber-500 hover:text-underline py-2 px-2" href="/tour">tour</a>
                             </li>
                         </ul>
                     </div>
