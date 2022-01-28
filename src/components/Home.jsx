dfec93bdcd6b06e8d91e87e0445bf9f200e7de83
allison <allison.durham@gmail.com>
2022-01-28 00:46:02 -0500

    padding

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,19 +4,21 @@ import image from "../assets/tiger.jpg";
 
 export default function Home() {
 
-  return (
-    <div className="home mt-16 mb-0 bg-yellow-700 py-24 md:p-10 lg:pt-6 lg:pb-20">
-      <div class="container bg-stone-800  rounded-xl">
-            <img
-              class="rounded-xl "
-              src={image}
-              alt=""
-            />
-            <p className="text-yellow-700 py-4">
-                Easy Tiger is our band, we play music real good!  people like us. one fan at a time!
-            </p>
-          </div>
-    </div>    
-  );
+    return (
+        <div className="home mt-24 md:mt-16 mb-0 pb-10 bg-yellow-700 ">
+            <div className="pt-8 px-4 md:p-10 lg:pt-6 pb-20">
+            <div class="container bg-stone-800  rounded-2xl ">
+                <img
+                    class="rounded-2xl "
+                        src={image}
+                        alt=""
+                    />
+                <p className="text-yellow-700 py-4">
+                    Easy Tiger is our band, we play music real good!  people like us. one fan at a time!
+                </p>
+            </div>      
+            </div>
+        </div>    
+    );
 }
 
